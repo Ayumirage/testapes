1c4310a9357f3e1da16d1a8021e6f3b3b793bd5c
vm.vdst <vm.vdst@gmail.com>
2022-02-18 12:16:52 +0100

    first commit

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -1,29 +1,30 @@
-import CollectionConfigInterface from '../lib/CollectionConfigInterface';
-import whitelistAddresses from './whitelist.json';
+import CollectionConfigInterface from "../lib/CollectionConfigInterface";
+import whitelistAddresses from "./whitelist.json";
 
 const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: "testapes",
+  tokenName: "testapes",
+  tokenSymbol: "HOOGA",
+  hiddenMetadataUri:
+    "ipfs://QmQmhnjjniDwmNXFDTRKMMUfmxZDJSyxNft4MdgnBGznWH/hidden.json",
+  maxSupply: 20,
   whitelistSale: {
-    price: 0.05,
+    price: 0.01,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
-    maxMintAmountPerTx: 2,
+    price: 0.02,
+    maxMintAmountPerTx: 1,
   },
   publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
+    price: 0.03,
+    maxMintAmountPerTx: 1,
   },
-  contractAddress: null,
-  openSeaSlug: 'my-nft-token',
+  contractAddress: "0x1aabd4c5501c8740c53c598c7a2baa5a1ed7fd09",
+  openSeaSlug: "my-nft-token",
   whitelistAddresses: whitelistAddresses,
 };
 
